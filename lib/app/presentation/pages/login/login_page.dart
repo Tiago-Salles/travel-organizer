c1e7e2075137d1fd94be9072378e8996667b29ac
Tiago <tiago.salles@houseasy.net>
2022-07-19 18:30:23 -0300

    Refactor: Removed MobX, Feat: Implemented ValueNotifier

--- lib/app/presentation/pages/login/login_page.dart
+++ lib/app/presentation/pages/login/login_page.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:travel_organizer/app/core/services/device_information_service.dart';
+import 'package:travel_organizer/app/presentation/presentation_resources/images/app_images.dart';
+
+enum ActionAnimation {
+  animate,
+  stoped,
+}
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -8,12 +15,142 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  ValueNotifier<Widget> animatedButton =
+      ValueNotifier<Widget>(const AnimatedButtonLoginPage(
+    actionAnimation: ActionAnimation.stoped,
+  ));
+
+  void changeButton() {
+    animatedButton = ValueNotifier<Widget>(const AnimatedButtonLoginPage(
+      actionAnimation: ActionAnimation.animate,
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("LoginPage"),
+    double screenPerimeter = DeviceInformationService.screenPerimeter(context);
+    return Scaffold(
+      backgroundColor: const Color(0xAA322F40),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: screenPerimeter * 0.06),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(width: screenPerimeter * 0.01),
+                Text(
+                  "Login",
+                  style: TextStyle(
+                    fontSize: screenPerimeter * 0.03,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: screenPerimeter * 0.05),
+            const AppTextFormField(hintText: "email"),
+            SizedBox(height: screenPerimeter * 0.01),
+            const AppTextFormField(hintText: "senha"),
+            SizedBox(height: screenPerimeter * 0.04),
+            GestureDetector(
+              onTap: () {
+                changeButton();
+              },
+              child: animatedButton.value,
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class AppTextFormField extends StatelessWidget {
+  final String hintText;
+  const AppTextFormField({Key? key, required this.hintText}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenPerimeter = DeviceInformationService.screenPerimeter(context);
+    return Container(
+      width: screenPerimeter * 0.13,
+      height: screenPerimeter * 0.025,
+      padding: const EdgeInsets.only(left: 10, right: 10),
+      decoration: BoxDecoration(
+        color: const Color(0xAA322F40),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Center(
+        child: TextFormField(
+          textAlignVertical: TextAlignVertical.center,
+          decoration: InputDecoration(
+            hintStyle: const TextStyle(color: Colors.grey),
+            contentPadding: EdgeInsets.zero,
+            border: const OutlineInputBorder(
+              borderSide: BorderSide.none,
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Color(0xAA322F40),
+              ),
+            ),
+            hintText: hintText,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AnimatedButtonLoginPage extends StatelessWidget {
+  final ActionAnimation actionAnimation;
+  const AnimatedButtonLoginPage({Key? key, required this.actionAnimation})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenPerimeter = DeviceInformationService.screenPerimeter(context);
+    if (actionAnimation == ActionAnimation.animate) {
+      return SizedBox(
+        height: screenPerimeter * 0.03,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            Positioned(
+              left: MediaQuery.of(context).size.width / 2 -
+                  (screenPerimeter * 0.04),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(60),
+                ),
+                height: screenPerimeter * 0.03,
+                width: screenPerimeter * 0.08,
+              ),
+            ),
+            Positioned(
+              left: MediaQuery.of(context).size.width / 2 -
+                  (screenPerimeter * 0.02),
+              child: SizedBox(
+                height: screenPerimeter * 0.03,
+                width: screenPerimeter * 0.04,
+                child: AppImages.airPlaneImage,
+              ),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return Container(
+        height: 30,
+        width: 30,
+        color: Colors.amber,
+      );
+    }
   }
 }
--- lib/app/presentation/pages/splash/splash_page.dart
+++ lib/app/presentation/pages/splash/splash_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:travel_organizer/app/core/navigation/app_navigator.dart';
 import 'package:travel_organizer/app/core/services/device_information_service.dart';
-import 'package:travel_organizer/app/presentation/animations/package_animations/lottie_animations.dart';
-import 'package:travel_organizer/app/presentation/images/app_images.dart';
+import 'package:travel_organizer/app/presentation/presentation_resources/animations/package_animations/lottie_animations.dart';
+import 'package:travel_organizer/app/presentation/presentation_resources/images/app_images.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -19,6 +19,7 @@ class _SplashPageState extends State<SplashPage> {
     double screenPerimeter = DeviceInformationService.screenPerimeter(context);
 
     return Scaffold(
+      backgroundColor: const Color(0xAA322F40),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -59,8 +60,8 @@ class _SplashPageState extends State<SplashPage> {
                             "Your Travel",
                             style: TextStyle(
                               fontSize: screenPerimeter * 0.015,
-                              color: Colors.black87,
-                              fontWeight: FontWeight.w300,
+                              color: Colors.blue,
+                              fontWeight: FontWeight.w700,
                             ),
                           ),
                         ),
